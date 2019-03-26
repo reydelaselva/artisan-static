131f00d6547863fe45c5e9bb5614f484bf17b0cf
reydelaselva <48965579+reydelaselva@users.noreply.github.com>
2019-03-26 14:00:39 -0700

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -6,6 +6,6 @@
     <h2>Asset Compilation Test</h2>
 
     <p>CSS test: <span class="test-css">text with border and padding</span></p>
-
+    <p>JavaScript test: <a href="../index.html" class="index">click me</a></p>
     <p>JavaScript test: <a href="#" class="test-js">click me</a></p>
 @endsection
